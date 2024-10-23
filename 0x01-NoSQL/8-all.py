63ee3c3f630321db2a039d656a578f9287235218
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-23 04:56:22 +0300

    Add list_all functoin that list all document in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,7 +3,7 @@
 
 def list_all(mongo_collection):
     '''lists all documents in a collection'''
-    if not mongo_collection.count_documents({}) == 0:
+    if mongo_collection.count_documents({}) == 0:
         return []
 
     return mongo_collection.find()
