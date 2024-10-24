7d6c0bc3d9f64dac17443498a9ccd429cc385e51
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-24 04:26:52 +0300

    update: add get, get_str and get_int methods for conversion

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Writing strings to Redis"""
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
 import redis
 
 
@@ -19,3 +19,27 @@ class Cache:
         random_key = str(uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float]:
+        '''takes a key string argument and an Callable argument named fn'''
+
+        value = self._redis.get(key)
+
+        if value is None:
+            return None
+
+        if fn:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        '''automatically parametrize Cache.get with conversion to string'''
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        '''automatically parametrize Cache.get with conversion to integer'''
+        value = self._redis.get(key)
+        return int(value.decode('utf-8'))
